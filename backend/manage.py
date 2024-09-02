bbfb69722076264565c6e0481902fb7e28aff86f
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-09-02 16:19:13 +0530

    Update the settings with auto runn the server with HOST and PORT set in .env file and not set it run the default HOST and PORT

--- backend/manage.py
+++ backend/manage.py
@@ -2,11 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
+    # Check for HOST and PORT using python-decouple
+    host = config("HOST", default="127.0.0.1")
+    port = config("PORT", default="8000")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +21,13 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Add the runserver command with the specified HOST and PORT
+    if len(sys.argv) == 1 or sys.argv[1] == "runserver":
+        sys.argv = sys.argv[:2] + [f"{host}:{port}"]
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
