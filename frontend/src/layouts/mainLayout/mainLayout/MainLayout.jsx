bc28972188558dcacc36dcb46ba5edc85f5b37cb
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-09-15 16:07:47 +0530

    Add a NavLink component for Return back and other link

--- frontend/src/layouts/mainLayout/mainLayout/MainLayout.jsx
+++ frontend/src/layouts/mainLayout/mainLayout/MainLayout.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../header/Header'
 import ReturnToTopButton from '../returnToTopButton/ReturnToTopButton'
 import Footer from '../footer/Footer'
+import NavLinks from '../navLink/NavLinks'
 
 export default function MainLayout() {
+
+    const { pathname } = useLocation()
+
     return (
         <>
             <Header />
             <main>
+                {
+                    !["/", "/home"].includes(pathname) && <NavLinks />
+                }
                 <Outlet />
             </main>
             <ReturnToTopButton />
