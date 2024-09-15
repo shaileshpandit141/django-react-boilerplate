db92e59a954175df3f510d0add1a1428b51ddb83
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-09-15 20:17:10 +0530

    Decode the JWT Token and store the user id in authSlice

--- frontend/src/features/auth/slices/authSlice.js
+++ frontend/src/features/auth/slices/authSlice.js
@@ -1,14 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit' 
-import { loginThunk, refreshAccessTokenThunk } from '../thunks/authThunk' 
+import { createSlice } from '@reduxjs/toolkit'
+import { jwtDecode } from 'jwt-decode';
+import { loginThunk, refreshAccessTokenThunk } from '../thunks/authThunk'
 
 // Initial State
 const initialState = {
     accessToken: localStorage.getItem('accessToken') || null,
     refreshToken: localStorage.getItem('refreshToken') || null,
     isAuthenticated: !!localStorage.getItem('refreshToken'),
+    id: localStorage.getItem('accessToken') ? jwtDecode(localStorage.getItem('accessToken'))?.user_id : null,
     status: 'idle',
     error: null,
-} 
+}
 
 // auth Slice
 const authSlice = createSlice({
@@ -16,47 +18,47 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         logout: (state) => {
-            state.accessToken = null 
-            state.refreshToken = null 
-            state.isAuthenticated = false 
-            localStorage.removeItem('accessToken') 
-            localStorage.removeItem('refreshToken') 
+            state.accessToken = null
+            state.refreshToken = null
+            state.isAuthenticated = false
+            localStorage.removeItem('accessToken')
+            localStorage.removeItem('refreshToken')
         },
     },
     extraReducers: (builder) => {
         builder
             .addCase(loginThunk.pending, (state) => {
-                state.status = 'loading' 
+                state.status = 'loading'
             })
             .addCase(loginThunk.fulfilled, (state, action) => {
-                state.status = 'succeeded' 
+                state.status = 'succeeded'
                 const { access, refresh } = action.payload
-                state.accessToken = access 
-                state.refreshToken = refresh 
-                state.isAuthenticated = true 
-                localStorage.setItem('accessToken', access) 
+                state.accessToken = access
+                state.refreshToken = refresh
+                state.isAuthenticated = true
+                localStorage.setItem('accessToken', access)
                 localStorage.setItem('refreshToken', refresh)
             })
             .addCase(loginThunk.rejected, (state, action) => {
-                state.status = 'failed' 
-                state.error = action.payload 
+                state.status = 'failed'
+                state.error = action.payload
             })
             .addCase(refreshAccessTokenThunk.fulfilled, (state, action) => {
-                state.status = 'succeeded' 
+                state.status = 'succeeded'
                 const { access } = action.payload
-                state.accessToken = access 
-                state.isAuthenticated = true 
-                localStorage.setItem('accessToken', access) 
+                state.accessToken = access
+                state.isAuthenticated = true
+                localStorage.setItem('accessToken', access)
             })
             .addCase(refreshAccessTokenThunk.rejected, (state) => {
-                state.accessToken = null 
-                state.refreshToken = null 
-                state.isAuthenticated = false 
-                localStorage.removeItem('accessToken') 
-                localStorage.removeItem('refreshToken') 
+                state.accessToken = null
+                state.refreshToken = null
+                state.isAuthenticated = false
+                localStorage.removeItem('accessToken')
+                localStorage.removeItem('refreshToken')
             })
     },
-}) 
+})
 
-export const { logout } = authSlice.actions 
+export const { logout } = authSlice.actions
 export default authSlice.reducer 
