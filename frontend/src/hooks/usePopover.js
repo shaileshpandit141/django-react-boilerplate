2fabdea034e43bcc980ff31461088536e52f4430
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-09-26 16:15:22 +0530

    Add a usePopover hook for simplyfi the use of popover or menu

---
+++ frontend/src/hooks/usePopover.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react"
+
+export function usePopover(popoverButtonRef, popoverRef) {
+
+  const [isVisible, setIsVisible] = useState(false)
+  const [initialVisibleStyle, setInitialVisibleStyle] = useState(
+    {
+      opacity: 1,
+      visibility: "visible",
+      transform: "scale(1)"
+    }
+  )
+  const [initialHiddenStyle, setInitialHiddenStyles] = useState(
+    {
+      opacity: 0,
+      visibility: "hidden",
+      transform: "scale(.98)"
+    }
+  )
+
+  // Toggle the popover visibility when button is clicked
+  const togglePopover = () => {
+    setIsVisible(prev => !prev)
+  }
+
+  // Close popover if clicked outside
+  useEffect(() => {
+    // Get all class names as an array from ref.current.classList
+    function getPopoverButtonClassName() {
+      if (popoverButtonRef.current) {
+        const classNamesArray = Array.from(popoverButtonRef.current.classList)
+        if (classNamesArray.length > 0) {
+          return classNamesArray[0]
+        } else {
+          throw new Error("set at least one class name in popover button.")
+        }
+      }
+      return ' '
+    }
+
+    const handleClickOutside = (event) => {
+      if (popoverRef.current
+        && !popoverRef.current.contains(event.target)
+        && !event.target.closest(`.${getPopoverButtonClassName()}`)) {
+        setIsVisible(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [popoverButtonRef, popoverRef, setIsVisible])
+
+  let popoverStyleWithState = isVisible ? initialVisibleStyle : initialHiddenStyle
+
+  useEffect(() => {
+    if ((initialHiddenStyle || initialVisibleStyle) && popoverRef.current) {
+      popoverRef.current.style["transition"] = "transform 0.3s ease-in-out, visibility 0.3s ease-in-out"
+      Object.entries(popoverStyleWithState).forEach(([key, value]) => {
+        popoverRef.current.style[key] = value
+      })
+    }
+  }, [initialHiddenStyle, initialVisibleStyle, popoverRef, popoverStyleWithState])
+ 
+  return { togglePopover, setInitialVisibleStyle, setInitialHiddenStyles }
+}
