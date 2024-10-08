55b54a7ddf87e197f54470052b6b1706d145527a
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-08 12:56:06 +0530

    Update the ErrorBoundary component

--- frontend/src/errors/errorBoundary/ErrorBoundary.jsx
+++ frontend/src/errors/errorBoundary/ErrorBoundary.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+import React from 'react'
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary'
 import './ErrorBoundary.scss'
-import { LazyMaterialIcon, icons } from 'lazyUtils/LazyMaterialIcon';
+import { LazyMaterialIcon, icons } from 'lazyUtils/LazyMaterialIcon'
 
 const ErrorFallback = ({ error, resetErrorBoundary }) => (
   <div className='inner-grid-2-2 error-boundary' role="alert">
     <div className="inner-error-boundary">
       <h4>Something went wrong</h4>
       <p>
-        <span>Message: </span>
-        {error.message}
+        {
+          error?.message && (
+            <>
+              <span>Message:</span>
+              {error.message}
+            </>
+          )
+        }
       </p>
       <button
         className='button'
         onClick={resetErrorBoundary}
       >
         <span className='icon'>
-          <LazyMaterialIcon iconName={icons.ReTry} />
+          <LazyMaterialIcon iconName={icons.reTry} />
         </span>
         <span className='label'>
           Try again
@@ -24,12 +30,12 @@ const ErrorFallback = ({ error, resetErrorBoundary }) => (
       </button>
     </div>
   </div>
-);
+)
 
 const ErrorBoundary = ({ children }) => (
   <ReactErrorBoundary FallbackComponent={ErrorFallback}>
     {children}
   </ReactErrorBoundary>
-);
+)
 
-export default ErrorBoundary;
+export default ErrorBoundary
