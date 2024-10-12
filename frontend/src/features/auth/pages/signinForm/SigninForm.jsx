9c05b0f59db1ca75c990857c6d3311a1d4ac5e2c
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-12 14:48:03 +0530

    Update the SigninForm toast message

--- frontend/src/features/auth/pages/signinForm/SigninForm.jsx
+++ frontend/src/features/auth/pages/signinForm/SigninForm.jsx
@@ -68,7 +68,7 @@ export default function SigninForm() {
       }
     }
     if (status === 'succeeded') {
-      toast.success('Sign-in successful! Redirecting...')
+      toast.success('Sign-in successful!')
     }
   }, [status, error])
 
