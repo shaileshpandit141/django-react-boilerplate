4a759231ab9a0c25507b5f2e9f2ca1c7b95b8305
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-20 12:37:39 +0530

    Update the sass styles and SearchService component

--- frontend/src/pages/searchService/SearchService.jsx
+++ frontend/src/pages/searchService/SearchService.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import './SearchServices.scss'
+import './SearchService.scss'
 import { Link } from "react-router-dom"
 import SearchBarForm from "components/specific/searchBarForm/SearchBarForm"
 import { LazyMaterialIcon, icons } from "lazyUtils/LazyMaterialIcon"
@@ -8,11 +8,10 @@ export default function SearchService() {
   return (
     <div className="inner-grid-2-2 search-services-page">
       <section className="search-bar-section">
-        <Link to='..' className="link">
+        <Link to='..' className="link-as-icon">
           <span className="icon">
             <LazyMaterialIcon iconName={icons.arrowBack} />
           </span>
-          <span className="label">Back</span>
         </Link>
         <SearchBarForm />
       </section>
