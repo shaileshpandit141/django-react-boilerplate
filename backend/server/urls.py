8268a50deaf3ad97fe06c989c17dfea7d260d3d6
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-11-10 12:19:26 +0530

    Override the 404 response with json response. if the endpoint is not found in server

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -6,14 +6,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import IndexTemplateView
+from django.conf.urls import handler404
+from .views import custom_404_view, IndexTemplateView
 from user_account import urls
+
 
 urlpatterns = [
     path("", IndexTemplateView.as_view()),
     path("admin/", admin.site.urls),
     path("api/auth/", include(urls)),
 ]
+
+# Setting the custom_404_view Handler.
+handler404 = custom_404_view
 
 # Allows the application to know what folder to access on the server side when receiving a request from the MEDIA_URL.
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/server/views.py
+++ backend/server/views.py
@@ -1,4 +1,16 @@
+from django.http import JsonResponse
 from django.views.generic import TemplateView
+
+
+def custom_404_view(request, exception=None):
+    return JsonResponse(
+        {
+          "status": 400,
+          "error": "Not Found",
+          "message": "The endpoint you requested was not found."
+        },
+        status=404
+    )
 
 
 class IndexTemplateView(TemplateView):
